023755612089e624f6bc94f596a6a5fc7c0ecd73
Koryukova <ekaterina.koryukova@gmail.com>
2018-02-14 22:47:08 +0300

    Add new reg file lesson 6

--- reg.php
+++ reg.php
@@ -1,12 +1,41 @@
 <?php
 
+define('UPLOAD_DIR', 'upload/');
+
+//$h = fopen('test.txt', 'a+');
+//for ($i=1;$i<=100;$i++) {
+//    fwrite($h, "new string: {$i}" . PHP_EOL);
+//}
+//fclose($h);
+
+//$arrResult = glob('*.txt');
+//foreach ($arrResult as $filename) {
+//    var_dump(filesize($filename));
+////    copy($filename, './css/'.$filename.'.bak');
+////    rename('./css/'.$filename.'.bak', './css/'.$filename);
+//    unlink('./css/'.$filename);
+//}
+//var_dump($arrResult);
+//
+//die();
+
+
 function getFullName($user) {
     return $user['firstname'] . ' ' . $user['lastname'];
+}
+
+function createPath($path) {
+    $isSuccess = false;
+    if (!file_exists($path)) {
+        $isSuccess = mkdir($path, 0777, true);
+    }
+    return $isSuccess;
 }
 
 $errorMessage = [];
 
 if ( isset($_POST['is_agree']) ) {
+    // Создание пользователя
     $user = [
         'firstname' => $_POST['firstname'],
         'lastname' => $_POST['lastname'],
@@ -17,12 +46,28 @@ if ( isset($_POST['is_agree']) ) {
         'stack_learn' => [],
         'list_fruits' => 'Яблоко, Апельсин, Груша',
     ];
-    var_dump(getFullName($user));
-    die();
     if (isset($_POST['stack_learn'])) {
         $user['stack_learn'] = $_POST['stack_learn'];
     }
 
+    $jsonUser = json_encode($user, JSON_UNESCAPED_UNICODE);
+    $arrUser = json_decode($jsonUser, true);
+    $objUser = json_decode($jsonUser, false);
+    $test = serialize(12);
+
+    var_dump($jsonUser);
+    var_dump($arrUser);
+    var_dump($objUser);
+    var_dump(unserialize($test));
+    die();
+
+    if (isset($_FILES['photo']) && empty($_FILES['photo']['error'])) {
+        $uploadPath = UPLOAD_DIR . 'photo/';
+        createPath($uploadPath);
+        move_uploaded_file($_FILES['photo']['tmp_name'], $uploadPath . uniqid() . '.png');
+    }
+
+    // Валидация
     if (!(strlen($user['firstname']) >= 3 && strlen($user['lastname'])) >= 3) {
         $errorMessage[] = 'Имя и Фамилия не должны бать короче 3х символов';
     }
@@ -35,6 +80,8 @@ if ( isset($_POST['is_agree']) ) {
     ) {
         $errorMessage[] = 'Требуется html и php';
     }
+
+    var_dump($user);
 }
 
 ?>
@@ -81,20 +128,24 @@ if ( isset($_POST['is_agree']) ) {
 
     <hr />
 
-    <form action="" method="POST">
+    <form action="" method="POST" enctype="multipart/form-data">
         <div class="form-group">
             <label for="firstname">Имя</label>
             <input class="form-control" id="firstname" name="firstname"
-                   value="<?= (isset($_POST['firstname'])) ? $_POST['firstname'] : '' ?>" placeholder="Имя">
+                   value="<?= (isset($_POST['firstname'])) ? $_POST['firstname'] : 'Тест' ?>" placeholder="Имя">
         </div>
         <div class="form-group">
             <label for="lastname">Фамилия</label>
             <input class="form-control" id="lastname" name="lastname"
-                   value="<?= (isset($_POST['lastname'])) ? $_POST['lastname'] : '' ?>" placeholder="Фамилия">
+                   value="<?= (isset($_POST['lastname'])) ? $_POST['lastname'] : 'Тест' ?>" placeholder="Фамилия">
         </div>
         <div class="form-group">
             <label for="password" class="control-label">Пароль</label>
             <input type="password" class="form-control" name="password" id="password" placeholder="Пароль">
+        </div>
+        <div class="form-group">
+            <label for="photo" class="control-label">Фото</label>
+            <input type="file" class="form-control" name="photo">
         </div>
         <div class="form-group">
             <label for="password" class="control-label">Пол:</label>
@@ -131,7 +182,7 @@ if ( isset($_POST['is_agree']) ) {
 
         </div>
         <div class="checkbox">
-            <label><input type="checkbox" name="is_agree" value="1" required> Условия соглашения</label>
+            <label><input type="checkbox" name="is_agree" value="1" checked required> Условия соглашения</label>
         </div>
         <button class="btn btn-primary">Зарегистрироваться</button>
     </form>
