88fd37a5b18286fb9a38fc185dbd77fa6ef1cb56
SkillUP Student <student@skillup.spb.ru>
2018-02-20 20:49:38 +0300

    Add head in new folder_lesson 3

--- users.php
+++ users.php
@@ -6,6 +6,20 @@ $result = $bd->query('SELECT * FROM users;');
 $users = $result->fetchAll();
 
 var_dump($users);
+
+$users = $result->fetchAll();
+
+$filename = 'counter.txt';
+
+if (file_exists($filename)) {
+    $counter = file_get_contents('counter.txt');
+} else {
+    $counter = 0;
+}
+$counter++;
+file_put_contents($filename, $result);
+
+
 ?>
 
 <!doctype html>
